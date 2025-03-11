66098fef2f62596779df8300fcf916f5fb9f7091
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-03-11 11:20:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log (3 
+    +
+    3) // this is not a good practice, console.log(3+3) should be a good practice, code readability should be high
+
+// dataTypes:
+
+let name = "Pranay"; // string
+let age = 21 // integer
+let isLoggedIn = false // boolean
+
+// notes : (primitive datatypes)
+// 1. number => 2 to power 53
+// 2. bigint
+// 3. string => " "
+// 4. boolean => true/false
+// 5. null => standalone value
+// 6. undefined =>  
+// 7. symbol => used mostly in react for uniquness    
+
+// object
+
+console.log(typeof "pranay");
+
+
+//question : 
+conosle.log(typeof null); //object   // null ? => null is object
+console.log(typeof undefined) // undefined  // undefined ? => it is a type of undefined
