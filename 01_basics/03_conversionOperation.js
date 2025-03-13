1201c8b8e2644004280cde536b380d6d59d4806d
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-03-13 09:10:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = "abc";
+// let score = 12;
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+/* basic conversion :
+Notes : 
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+
+// let isLoggedIn = 1 => true
+// let isLoggedIn = 0 => false
+// let isLoggedIn = "" => false
+// let isLoggedIn = "Pranay" => true
+
+// let booelanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booelanIsLoggedIn);
+
+
+// let someNumber = 33 => string
+// let someNumber = "Pranay"; => string
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
