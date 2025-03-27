f8a2731dfcc4b10877cab9edcac0ebfd897de416
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-03-27 11:15:07 +0000

    datatypes summary explain

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive : call by vlaue
+// 7 types : String, number, boolean, null, undefiened, symbol, BigInt
+
+
+// is javascript statically typed  or dynamically typed ??
+
+
+
+
+// Non-Primitive (also known as reference)
+
+// Array, Objects, functions
