ff4dc413f3f919a6de39f89f545c8e66f97e2032
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-03-28 10:21:23 +0000

    nums and math cover

---
+++ 01_basics/08_nums_and_math.js
@@ -0,0 +1,34 @@
+// nums and maths
+
+const score = 21;
+// console.log(score);
+
+const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  // for example : e-commerece application
+
+
+const otherNumber = 1234.789
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// ****************MATHS**********************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log((Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
