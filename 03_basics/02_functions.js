224b17a50b0d0b919266b7eac477fdb759e800d7
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-04-05 08:11:31 +0000

    part 2 done

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,50 @@
+
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2));
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 600));
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 600));
+
+
+// const user = {
+//     username: "pranay",
+//     price: 199
+// }
+
+// function handleObject (anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+// }
+
+// handleObject(user);
+// handleObject({
+//     username: "codeguy",
+//     price: 399,
+// });
+
+const myNewArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+    
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
+
+
+
+
