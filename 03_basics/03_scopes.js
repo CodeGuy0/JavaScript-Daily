61177dc154d2c6bbe435353cccb060fb18981399
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-04-30 07:39:08 +0000

    changes done

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -15,11 +15,65 @@
 
 
  //if I run code outside it will be global scope
-if (true){ // block scope
-    let a = 10;
-    const b = 20;
-    var c = 30;
-}
+// if (true){ // block scope
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
 //console.log(a); //undefined
 //console.log(b); // undefined 
 //console.log(c); // 30
+
+// example 1 : 
+// function one () {
+//     const username = "pranay"
+
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     two()
+// }
+
+// one()
+// // two()
+
+
+
+// example 2:
+
+// if (true) {
+//     const username = "pranay"
+//     if (username === "pranay") {
+//         const website = " youtube"
+//         console.log(username + website); // it will access....
+
+//     }
+//     // console.log(website); // it will not access 
+// }
+
+// console.log(username); // it will also not access 
+
+// NOTE : it will all about closure 
+
+
+// *************************** INTERESTING *********************************
+
+function addone(num){  // it is called function
+    return num + 1
+}
+
+addone(5)
+
+
+const addTwo = function(num){  // it is also  called function but sometimes it is called expression
+    return num + 2
+}
+
+addTwo(5)
+
+
+
+
